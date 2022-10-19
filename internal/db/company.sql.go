8d74713995ab2eea4d26e320e8d7a066b563c498
jerickmolina <jericknmolina@protonmail.com>
2022-10-19 08:32:11 -0500

    Added a unit test and more methods

--- internal/db/company.sql.go
+++ internal/db/company.sql.go
@@ -2,17 +2,49 @@ package db
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
 )
 
-const createCompany = `insert into Company(Name) values (?)
+const createCompany = `insert into Company(Name,UniqueId) values (?,?)
 `
 
-func (q *Queries) CreateCompany(context context.Context, companyName string) error {
+func (q *Queries) CreateCompany(context context.Context, companyName string, uniqueId string) error {
 
-	_, err := q.db.ExecContext(context, createCompany, companyName)
+	_, err := q.db.ExecContext(context, createCompany, companyName, uniqueId)
 	if err != nil {
 		return err
 	}
+
+	return nil
+}
+
+const findCompanyWithUniqueId = `select CompanyId from Company where UniqueId = ?
+`
+
+func (q *Queries) FindCompanyWithUniqueId(ctx context.Context, uniqueId string) (int, error) {
+	var id int
+	response := q.db.QueryRowContext(ctx, findCompanyWithUniqueId, uniqueId).Scan(&id)
+	if response != nil {
+		fmt.Print(response)
+		return -1, response
+	}
+	return id, nil
+}
+func (q *Queries) ValidateUniqueId(ctx context.Context, uniqueId string) error {
+	var r string
+	response := q.db.QueryRowContext(ctx, findCompanyWithUniqueId, uniqueId).Scan(&r)
+	if response == sql.ErrNoRows {
+		return response
+	}
+	return nil
+}
+
+// TODO: Create a company key when a Authorized User makes a invite code that has 3 params
+// What company
+// EXP time
+// Has key been used?
+func (q *Queries) JoinCompanyByInvitation(ctx context.Context, companyKey string) error {
 
 	return nil
 }
--- internal/db/company_test.go
+++ internal/db/company_test.go
@@ -3,19 +3,31 @@ package db
 import (
 	"context"
 	"eRecord/util"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func createRandomCompany(t *testing.T) {
-
-	err := testQueries.CreateCompany(context.Background(), util.RandomCompany(5))
+func createRandomCompany(t *testing.T) string {
+	uniqueId := util.RandomChars(10)
+	err := testQueries.CreateCompany(context.Background(), "Test_"+util.RandomCompany(5), uniqueId)
 
 	require.NoError(t, err)
-	return
+	return uniqueId
 }
 
 func TestCreateCompany(t *testing.T) {
 	createRandomCompany(t)
+}
+
+func TestFindCompanyWithUniqueId(t *testing.T) {
+	uniqueId := createRandomCompany(t)
+
+	id, err := testQueries.FindCompanyWithUniqueId(context.Background(), uniqueId)
+	var invalidNum int = -1
+
+	require.NoError(t, err)
+	fmt.Print(id)
+	require.GreaterOrEqual(t, id, invalidNum, "Errr!?")
 }
